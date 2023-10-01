fae5cdefc87b92fd61d247bee90432c1712cae18
stoykovnet <stoykovnet@gmail.com>
2023-10-01 15:59:55 +0200

    Introduce PetService class to abstract business logic from the API controller and thus leave only data validation and status codes in the controller and other endpoint specifics.

---
+++ PetStore.Services/Interfaces/IPetService.cs
@@ -0,0 +1,14 @@
+﻿using PetStore.DataAccessLayer.Models;
+
+namespace PetStore.Services.Interfaces
+{
+    public interface IPetService
+    {
+        public void AddPet(PetModel entity);
+        public void DeletePet(long? id);
+        public void FindPetsByTags(long petId);
+        public void GetPetById(IEnumerable<string> tags);
+        public void UpdatePet(PetModel pet);
+        public void UpdatePetWithForm(long petId, string name, string status);
+    }
+}
---
+++ PetStore.Services/PetService.cs
@@ -0,0 +1,48 @@
+﻿using PetStore.DataAccessLayer.Models;
+using PetStore.DataAccessLayer.Repositories.Interfaces;
+using PetStore.Services.Interfaces;
+
+namespace PetStore.Services
+{
+    public class PetService : IPetService
+    {
+        private IPetRepository petRepository;
+        private string mockIdentifier = Guid.NewGuid().ToString();
+        private string mockEmail = "a@a.a";
+
+        public PetService(IPetRepository petRepository)
+        {
+            this.petRepository = petRepository;
+        }
+
+        public void AddPet(PetModel entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeletePet(long? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FindPetsByTags(long petId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetPetById(IEnumerable<string> tags)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePet(PetModel pet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePetWithForm(long petId, string name, string status)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
